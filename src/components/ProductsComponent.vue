a9429d9581148ee5491ef753d04084f18ec3206e
LEONARDO RIBEIRO DE ASSIS <leoribeiroassis@gmail.com>
2025-05-27 13:06:31 -0300

    API conectada

---
+++ src/components/ProductsComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="rounded-lg p-4 text-black shadow-md bg-gray-100">
+    <img
+      :src="product.thumbnail"
+      :alt="product.title"
+      class="mx-auto h-32 object-contain mb-4"
+    />
+    <h2 class="text-left capitalize font-bold text-black">
+      {{ product.title }}
+    </h2>
+    <p class="text-sm text-gray-600">{{ product.category }}</p>
+
+    <p class="my-4 text-gray-700">{{ product.description }}</p>
+
+    <div class="space-x-2 flex items-left">
+      <span class="text-green-600 font-bold">$ {{ product.price }}</span>
+      <span class="text-yellow-500">⭐ {{ product.rating }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+    defineProps({
+    product: Object
+    });
+</script>
---
+++ src/views/PrductsView.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-bold text-center mb-6"> Produtos</h1>
+
+        <div class="grid grid-cols-2 md:grid-cols-4 lg:-grid-cols-6">
+            <ProductCard
+                v-for="p in products"
+                :key="p.name"
+                :product="p"
+            />
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import {ref, onMounted} from 'vue'
+    import axios from 'axios'
+    import ProductCard from '../components/ProductsComponent.vue'
+
+    const products = ref([])
+
+    const offset = ref(0)
+    const limit = 6
+
+    const api = axios.create({
+        baseURL: 'https://dummyjson.com',
+    });
+
+    const getProducts = async() => {
+        try {
+            const response = await api.get(`/products?limit=${limit}&skip=${offset.value}`);
+            const results = response.data.products
+
+            console.log(results)
+
+            products.value = results.map(product => ({
+                title: product.title,
+                category: product.category,
+                description: product.description,
+                thumbnail: product.thumbnail,
+                price: product.price,
+                rating: product.rating,
+        }))
+       console.log(products)
+    }
+            
+        catch (error) {
+            console.error('Erro ao buscar produtos:', error.message)
+        }
+    }
+
+    
+    onMounted(getProducts)
+</script>
